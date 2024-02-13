de91dc51ad7f746fbc398b922a140e3512ab8a75
Filipe Brochier <filipe.brochier@gmail.com>
2024-02-13 14:37:21 -0300

    feat: Add an abstract repository to default mongoose methods

---
+++ libs/common/src/database/abstract.repository.ts
@@ -0,0 +1,58 @@
+import { FilterQuery, Model, Types, UpdateQuery } from 'mongoose';
+import { AbstractDocument } from './abstract.schema';
+import { Logger, NotFoundException } from '@nestjs/common';
+
+export abstract class AbstractRepository<TDocument extends AbstractDocument> {
+  protected abstract readonly logger: Logger;
+
+  constructor(protected readonly model: Model<TDocument>) {}
+
+  async create(document: Omit<TDocument, '_id'>): Promise<TDocument> {
+    const newDocument = new this.model({
+      ...document,
+      _id: new Types.ObjectId(),
+    });
+
+    const createdDocument = await newDocument.save();
+    return createdDocument.toJSON() as unknown as TDocument;
+  }
+
+  async find(filterQuery: FilterQuery<TDocument>): Promise<TDocument[]> {
+    return this.model.find(filterQuery).lean<TDocument[]>(true);
+  }
+
+  async findOne(filterQuery: FilterQuery<TDocument>): Promise<TDocument> {
+    const document = await this.model
+      .findOne(filterQuery)
+      .lean<TDocument>(true);
+
+    if (!document) {
+      this.logger.error('Document not found', filterQuery);
+      throw new NotFoundException('Document not found');
+    }
+
+    return document;
+  }
+
+  async findOneAndUpdate(
+    filterQuery: FilterQuery<TDocument>,
+    update: UpdateQuery<TDocument>,
+  ): Promise<TDocument> {
+    const document = await this.model
+      .findOneAndUpdate(filterQuery, update, { new: true })
+      .lean<TDocument>(true);
+
+    if (!document) {
+      this.logger.error('Document not found', filterQuery);
+      throw new NotFoundException('Document not found');
+    }
+
+    return document;
+  }
+
+  async findOneAndDelete(
+    filterQuery: FilterQuery<TDocument>,
+  ): Promise<TDocument> {
+    return this.model.findOneAndDelete(filterQuery).lean<TDocument>(true);
+  }
+}
