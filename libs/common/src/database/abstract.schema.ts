bc4bd2c1cecf832c67d9d1ef18969b2d5a80061a
Filipe Brochier <filipe.brochier@gmail.com>
2024-02-13 22:01:25 -0300

    feat: Implements create-reservation.dto.ts, controller and service

--- libs/common/src/database/abstract.schema.ts
+++ libs/common/src/database/abstract.schema.ts
@@ -3,6 +3,6 @@ import { SchemaTypes, Types } from 'mongoose';
 
 @Schema()
 export class AbstractDocument {
-  @Prop({ TYPE: SchemaTypes.ObjectId })
+  @Prop({ type: SchemaTypes.ObjectId })
   _id: Types.ObjectId;
 }
