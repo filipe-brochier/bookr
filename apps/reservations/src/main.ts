037e256a79ddd448b8f7ed2fc9a5b100b2e953dd
Filipe Brochier <filipe.brochier@gmail.com>
2024-03-06 16:57:13 -0300

    feat: Add fastify adapter to reservations app

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -2,15 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { ReservationsModule } from './reservations.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(ReservationsModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    ReservationsModule,
+    new FastifyAdapter(),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
   app.useLogger(app.get(Logger));
-  await app.listen(3000);
+  await app.listen(4444);
 }
 bootstrap();
