e8a06068a9f05346ed005606926342792c627ab1
Filipe Brochier <filipe.brochier@gmail.com>
2024-03-08 14:24:18 -0300

    feat: Create user model and repository

---
+++ apps/auth/src/users/users.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post()
+  async createUser(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+}
---
+++ apps/auth/src/users/users.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Injectable()
+export class UsersService {
+  async create(createUserDto: CreateUserDto) {
+    return;
+  }
+}
