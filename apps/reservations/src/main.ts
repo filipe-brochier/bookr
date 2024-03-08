3df20dc991425043d50fe7ab32f085477dc0b8cb
Filipe Brochier <filipe.brochier@gmail.com>
2024-03-08 13:57:45 -0300

    fix: Fix Dockerfile for reservations

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -2,22 +2,26 @@ import { NestFactory } from '@nestjs/core';
 import { ReservationsModule } from './reservations.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     ReservationsModule,
     new FastifyAdapter(),
   );
+
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
+
   app.useLogger(app.get(Logger));
-  await app.listen(4444);
+
+  await app.listen(3000, '0.0.0.0'); // Ouvindo em todas as interfaces de rede
 }
 bootstrap();
